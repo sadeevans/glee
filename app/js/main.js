c428360527adbf1cacd008756717c47bf1985214
sadeevans <che2305@mail.ru>
2022-06-20 07:58:46 +0300

    added catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function() {
+  
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').toggleClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function() {
+    $('.product-item').addClass('product-item--list');
+      
+  })
+
+  $('.button-grid').on('click', function() {
+    $('.product-item').removeClass('product-item--list');
+      
+  })
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -9,6 +24,31 @@ $(function() {
 });
 
 $(function() {
+	$('.select-style').styler();
+});
+
+
+$(function() {
   var mixer = mixitup('.categories__items');
+});
+
+$(function() {
+ $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 100,
+    to: 500,
+    step: 5,
+    prefix: "$",
+     onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+     },
+      onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+        },
+    });
 });
 
